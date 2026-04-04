516d8d5e9cf3b5e4f2ae847eef6aee7db003ad33
Subham Mondal <subhamoffi@gmail.com>
2026-04-04 22:05:01 +0530

    Created the button for every page in homepage dynamically

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
-import Image from "next/image";
+import fs from "fs";
+import path from "path";
+import Link from "next/link";
 
-export default function Home() {
-  return (
-    <div>hello world</div>
+export default async function Home() {
+    const directory = path.join(process.cwd(), "src/app");
 
-  );
+    const folders = fs
+        .readdirSync(directory, { withFileTypes: true })
+        .filter((dirent) => {
+            if (dirent.isDirectory() && !dirent.name.startsWith("_")) return true;
+            return false;
+        })
+        .map((dirent) => dirent.name);
+
+    return (
+        <div className="flex gap-10 p-2">
+            {folders.map((name) => {
+                return (
+                    <Link href={name} key={name}>
+                        <button className="bg-neutral-800 px-3 py-2 rounded-lg cursor-pointer">
+                            {name}
+                        </button>
+                    </Link>
+                );
+            })}
+        </div>
+    );
 }
